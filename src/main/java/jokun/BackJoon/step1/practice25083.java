6b66d5addb7e99c82dfd6ed622f38c4a0c0db264
joanna930224 <annavu0224@gmail.com>
2022-08-11 13:56:49 +0900

    [STEP1] 입출력과 사칙연산 [25083] (completion)

---
+++ src/main/java/jokun/BackJoon/step1/practice25083.java
@@ -0,0 +1,13 @@
+package jokun.BackJoon.step1;
+
+
+public class practice25083 {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+}
