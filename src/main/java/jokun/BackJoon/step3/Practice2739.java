becdbcc5d6443713a7cc0a036410339d1443573c
joanna930224 <annavu0224@gmail.com>
2022-08-16 23:28:16 +0900

    [STEP3] 조건문 [2739] (ongoing)

---
+++ src/main/java/jokun/BackJoon/step3/Practice2739.java
@@ -0,0 +1,15 @@
+package jokun.BackJoon.step3;
+
+import java.util.Scanner;
+
+public class Practice2739 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(num + " * " + i + " = " + (num * i));
+        }
+    }
+}
