a1f539dc4f37e9b8ff423afcccf2d8b327dbf334
joanna930224 <annavu0224@gmail.com>
2022-09-10 09:27:57 +0900

    [STEP6] 문자열 [11654] (ongoing)

---
+++ src/main/java/jokun/BackJoon/step6/Practice11654.java
@@ -0,0 +1,15 @@
+package jokun.BackJoon.step6;
+
+import java.util.Scanner;
+
+public class Practice11654 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        char num = sc.next().charAt(0);
+
+        int ascii = (int)num;
+        System.out.println(ascii);
+    }
+}
+
