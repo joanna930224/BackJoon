94b426e29bdc038c1b66522f2a34d2e514bd8022
joanna930224 <annavu0224@gmail.com>
2022-09-16 11:56:33 +0000

    [STEP7] 기본 수학 1 : 부녀회장이 될테야 [2775] (ongoing)

--- src/main/java/jokun/BackJoon/step7/practice2775.java
+++ src/main/java/jokun/BackJoon/step7/practice2775.java
@@ -12,8 +12,8 @@ public class practice2775 {
         int[][] answer = new int[size][2];
 
         for(int i = 0; i < size; i++) {
-            answer[i][0] = input.nextInt(); // k층
-            answer[i][1] = input.nextInt(); // n호
+            answer[i][0] = input.nextInt();
+            answer[i][1] = input.nextInt();
         }
 
         int[][] apartment = new int[15][15];
@@ -21,6 +21,19 @@ public class practice2775 {
             apartment[0][i] = i;
         }
 
+        for(int i = 1; i < apartment.length; i++) {
+            for(int j = 1; j < apartment.length; j++) {
+                apartment[i][j] = apartment[i][j-1] + apartment[i-1][j];
+//				System.out.print(apartment[i][j] + " ");
+            }
+//			System.out.println();
+        }
+
+        for(int i = 0; i < answer.length; i++) {
+            System.out.println(apartment[answer[i][0]][answer[i][1]]);
+        }
+        input.close();
     }
 }
+
 
