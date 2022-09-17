e2581797ce64927d135c5e9c6e9618ec95f0b602
joanna930224 <annavu0224@gmail.com>
2022-09-17 22:40:53 +0900

    [STEP6] 문자열 [1157] (ongoing)

---
+++ src/main/java/jokun/BackJoon/step6/Practice1157.java
@@ -0,0 +1,13 @@
+package jokun.BackJoon.step6;
+
+import java.util.Scanner;
+
+public class Practice1157 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+
+
+    }
+}
