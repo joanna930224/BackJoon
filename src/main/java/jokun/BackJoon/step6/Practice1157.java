378009c53d832c208e01f17e257b14dbdfa817cb
joanna930224 <annavu0224@gmail.com>
2022-09-19 20:25:43 +0900

    [STEP6] 문자열 : 단어 공부 [1157] (ongoing)

--- src/main/java/jokun/BackJoon/step6/Practice1157.java
+++ src/main/java/jokun/BackJoon/step6/Practice1157.java
@@ -1,5 +1,6 @@
 package jokun.BackJoon.step6;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Practice1157 {
@@ -7,14 +8,26 @@ public class Practice1157 {
 
         Scanner sc = new Scanner(System.in);
         String text = sc.nextLine();
+        text = text.toLowerCase();
+        int max = 0;
+        char answer = ' ';
 
-        char[] textList = new char[text.length()];
+        int[] textList = new int[26];
+
         for (int i = 0; i < text.length(); i++) {
-            textList[i] = text.charAt(i);
+            textList[text.charAt(i)-'a']++;
+        }
+        System.out.println(Arrays.toString(textList));
+
+        for (int i = 0; i < textList.length; i++) {
+            if(max<textList[i]) {
+                max = textList[i];
+                answer = (char)(65+i);
+            }else if(max == textList[i]) {
+                answer='?';
+            }
         }
 
-        for (int i = 0; i < ; i++) {
-            
-        }
+        System.out.println(answer);
     }
 }
