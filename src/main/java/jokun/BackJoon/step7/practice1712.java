4a4865fe34c0842822d6efe71698608afa813fa7
joanna930224 <annavu0224@gmail.com>
2022-09-26 21:25:48 +0900

    [STEP7] 기본 수학 1 : 손익분기점 [1712] (ongoing)

---
+++ src/main/java/jokun/BackJoon/step7/practice1712.java
@@ -0,0 +1,22 @@
+package jokun.BackJoon.step7;
+
+import java.util.Scanner;
+
+public class practice1712 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+
+        if(num2>=num3) {
+            System.out.println(-1);
+        }
+        else {
+            System.out.println(num1/(num3-num2)+1);
+        }
+    }
+}
