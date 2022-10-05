848c06f2ca1e19b036ec7c7c4bc65ea80d25dc41
joanna930224 <annavu0224@gmail.com>
2022-10-05 16:02:17 +0900

    [STEP7] 기본 수학 1 : 설탕 배달 [2839] (ongoing)

--- src/main/java/jokun/BackJoon/step7/practice2839.java
+++ src/main/java/jokun/BackJoon/step7/practice2839.java
@@ -11,8 +11,22 @@ public class practice2839 {
         int pack = 0;
 
         if (3 <= Kilogram && Kilogram <= 5000) {
+            while(true){
+                if(Kilogram%5==0)
+                {
+                    System.out.println(Kilogram/5 + pack);
+                    break;
+                }else if(Kilogram<=0)
+                {
+                    System.out.println(-1);
+                    break;
+                }
+                Kilogram-=3;
+                pack++;
+            }
+        }else{
 
-
+            System.out.println(-1);
         }
     }
 }
